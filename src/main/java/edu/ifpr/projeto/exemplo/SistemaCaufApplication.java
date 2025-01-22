4a4d1cf880684649d39226057ab1c16a312b366e
Victor Henrique <41922049+Squivoix@users.noreply.github.com>
2025-01-22 14:07:10 -0300

    Add files via upload

---
+++ src/main/java/edu/ifpr/projeto/exemplo/SistemaCaufApplication.java
@@ -0,0 +1,13 @@
+package edu.ifpr.projeto.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCaufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCaufApplication.class, args);
+	}
+
+}
